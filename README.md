a9ed703e59447b89738d6cf9f4754e84d5ad1c03
Joseph Stroman <Joseph@scholasticits-MacBook-Pro-2.local>
2014-11-13 18:16:10 -0500

    Add helpful print messages, and break agination off into a class

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ The Movie Aginator
 -----
 This is a program that will look through all movies showing in theaters, and calculate the average age of the cast for each movie. 
 
-It uses a few classes to break up the work load into more manageable forms in case parameters that can't be controlled are changed. 
+It uses a class to break up the work load into sensible parts. 
 
-The movie list will come from fandango, the actor/actresses will come from imdb and the ages will come from wikipedia.
+The movie list will come from fandango, the actor/actresses list and their ages will come from imdb.
 
 Comments will be made throughout the code to provide further explanation.
--- code/aginator.py
+++ code/aginator.py
@@ -7,29 +7,37 @@ import re
 from bs4 import BeautifulSoup
 
 #constants
-TODAY = datetime.datetime.now()
 IMDB_BASE_URL = "http://www.imdb.com"
 FANDANGO_BASE_URL = "http://www.fandango.com"
-IMDB_DATE_FORMAT = "%Y-%m-%d"
+TODAY = datetime.datetime.now()
 
+class imdb_aginator:
+	def __init__(self, title):
+		self.total_cast = 0
+		self.total_age = 0
+		self.title = title
+		self.age = 0
+		self.date_format = "%Y-%m-%d"
+		self.today = datetime.datetime.now()
 
-def souper(url):
-	#opens url, and parses the html for searching and extracting
-	response = urllib2.urlopen(url)
-	page_source = response.read()
-	soup = BeautifulSoup(page_source)
-	return soup
+	def __str__(self):
+		return "The average age for the movie %s is %d" % (self.title, self.age) 
 
-def find_age(birthdate):
-	birthdate = datetime.datetime.strptime(birthdate, IMDB_DATE_FORMAT)
-	age = TODAY.year - birthdate.year
-	if ((TODAY.month, TODAY.day) < (birthdate.month, birthdate.day)):
-		age -= 1
-	return age
+	def aginate(self):
+		self.age = self.total_age/self.total_cast
+
+	def find_age(self, birthdate):
+		birthdate = datetime.datetime.strptime(birthdate, self.date_format)
+		age = self.today.year - birthdate.year
+		if ((self.today.month, self.today.day) < (birthdate.month, birthdate.day)):
+			age -= 1
+		return age
+		 
 
 def get_cast(movie):
 	#find all members of the cast
 	#adding the year in case of duplicate movie name/movie reboot
+	print 'processing.....'
 	url = IMDB_BASE_URL + "/find?q=" + movie + "+" + str(TODAY.year) + "&s=tt"
 	imdb_soup = souper(url)
 	titles_div = imdb_soup.find(class_="findList")
@@ -40,8 +48,7 @@ def get_cast(movie):
 	imdb_soup = souper(url)
 	cast_table = imdb_soup.find(class_="cast_list")
 	cast_tds = cast_table.find_all("td", itemprop="actor")
-	total_age = 0
-	cast_members = 0
+	aginator = imdb_aginator(movie)
 	for td in cast_tds:
 		m = re.search('nm[0-9]+', td.a['href'])
 		actor_id = m.group(0)
@@ -49,24 +56,34 @@ def get_cast(movie):
 		imdb_soup = souper(url)
 		try:
 			birthdate = imdb_soup.find('time')['datetime']
-			total_age += find_age(birthdate)
-			cast_members += 1
+			aginator.total_age += aginator.find_age(birthdate)
+			aginator.total_cast += 1
 		except:
-			print "Error: no birthdate on file, skipping cast member"
-	average_age = total_age/cast_members
-	print "The average age for the movie %s is %f" % (movie, average_age)  
-
+			#skip actor that has no birthday on file
+			pass
+	aginator.aginate()
+	print aginator
 
 
 def begin():
 	fandango_soup = souper("http://www.fandango.com/moviesintheaters")
 	movies_ul = fandango_soup.find(id="now_playing_np_div").find('ul')
-	movie_list = []
+	num_movies = len(movies_ul)
+	counter = 0
+	print "Total movies showing in theatres: %d" % (num_movies)
 	for li in movies_ul.find_all('li', recursive=False):
 		title = li.find(class_="visual-title").text.strip()
 		get_cast(title)
-		break
-	print "done"
+		counter += 1
+		print "Finished %d/%d movies" % (counter, num_movies)
+
+
+def souper(url):
+	#opens url, and parses the html for searching and extracting
+	response = urllib2.urlopen(url)
+	page_source = response.read()
+	soup = BeautifulSoup(page_source)
+	return soup
 
 
 if __name__ == "__main__":
